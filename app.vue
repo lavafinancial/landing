c42d715c37378169a1ddd83d0ae7b0e6d703248e
lorado <eugen.sacharow@gmx.de>
2022-03-20 03:15:41 +0100

    fix SEO and font

--- app.vue
+++ app.vue
@@ -2,6 +2,16 @@
   <NuxtLayout>
     <NuxtPage/>
   </NuxtLayout>
+
+  <Head>
+    <Title>LAVA Financial</Title>
+    <Link rel="icon" type="image/png" sizes="16x16" href="/favicon-16x16.png" />
+    <Link rel="icon" type="image/png" sizes="32x32" href="/favicon-32x32.png" />
+    <Link rel="icon" type="image/png" sizes="96x96" href="/favicon-96x96.png" />
+    <Link rel="shortcut icon" sizes="96x96" href="/favicon-96x96.png" />
+    <Link rel="icon" type="image/x-icon" href="/favicon.ico" />
+    <Link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Poppins:wght@300;400;500;600;700&display=swap" />
+  </Head>
 </template>
 
 <script lang="ts" setup>
