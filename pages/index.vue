5d436f4e5cd4947a0c5ed7d36e4e3a745783fed7
lorado <eugen.sacharow@gmx.de>
2022-03-20 04:06:37 +0100

    update text

--- pages/index.vue
+++ pages/index.vue
@@ -10,7 +10,7 @@
       <IconsLavaLogo class="ml-3.5 mb-4 w-24 h-24"/>
       <h1 class="text-white text-4xl md:text-6xl font-bold">Lava Financial</h1>
       <h2 class="mt-8 md:mt-12 text-lava-red text-2xl md:text-[42px] leading-tight font-semibold max-w-xs md:max-w-lg">Welcome to the most explosive place in DeFi.</h2>
-      <p class="mt-4 md:mt-6 text-lava-red md:text-xl">Launch coming soon</p>
+      <p class="mt-4 md:mt-6 text-lava-red md:text-xl">Website & Launch coming soon</p>
 
       <div class="mt-8 md:mt-12 flex items-start flex-col space-y-6 lg:flex-row lg:items-center lg:space-x-8 lg:space-y-0">
         <BaseButton target="_blank" href="/lava_whitepaper.pdf" type="primary" size="big">Whitepaper</BaseButton>
