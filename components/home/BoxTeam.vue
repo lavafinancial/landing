064e6a70b12ac646e0db4610944eb01f2d624814
lorado <eugen.sacharow@gmx.de>
2022-04-20 14:24:02 +0200

    make fotos glow

--- components/home/BoxTeam.vue
+++ components/home/BoxTeam.vue
@@ -9,7 +9,7 @@
           class="space-y-12 sm:grid sm:grid-cols-2 sm:gap-x-8 sm:gap-y-12 sm:space-y-0 lg:grid-cols-3 lg:gap-x-12">
         <li v-for="person in people" :key="person.name" class="">
           <div class="space-y-4">
-            <img class="object-cover shadow-lg rounded-4 w-full" :src="person.imageUrl" alt=""/>
+            <img class="object-cover rounded-4 w-full border border-lava-yellow glow-shadow-yellow" :src="person.imageUrl" alt=""/>
 
             <div class="space-y-2">
               <div class="text-lg leading-6 font-medium space-y-1">
